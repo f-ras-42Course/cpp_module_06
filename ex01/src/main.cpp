8275807ed3689e074020de8dbae8805580079388
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2025-09-03 10:24:19 +0200

    removed flushing

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char **argv) {
 	if (argc != 4) {
-		std::cout << "=== Serializer Test ===" << std::endl << std::endl;
-		std::cerr << "Usage: " << argv[0] << " <id> <message> <value>" << std::endl;
-		std::cerr << "Example: " << argv[0] << " 69 \"Some Message\" 3.14159265358" << std::endl;
+		std::cout << "=== Serializer Test ===\n\n";
+		std::cerr << "Usage: " << argv[0] << " <id> <message> <value>\n";
+		std::cerr << "Example: " << argv[0] << " 69 \"Some Message\" 3.14159265358\n";
 		return 1;
 	}
 
@@ -18,13 +18,11 @@ int main(int argc, char **argv) {
 	std::cout << "  Pointer Address: " << originalPtr << "\n";
 	std::cout << "  ID: " << originalData.id_ << "\n";
 	std::cout << "  Message: " << originalData.message_ << "\n";
-	std::cout << "  Value: " << originalData.value_ << "\n";
-	std::cout << std::endl;
+	std::cout << "  Value: " << originalData.value_ << "\n\n";
 	
 	uintptr_t serialized = Serializer::serialize(originalPtr);
 	std::cout << "Value of serialized pointer: " << serialized << "\n";
-	std::cout << "Serialized value in hex: 0x" << std::hex << serialized << std::dec << "\n";
-	std::cout << std::endl;
+	std::cout << "Serialized value in hex: 0x" << std::hex << serialized << std::dec << "\n\n";
 	
 	Data* deserializedPtr = Serializer::deserialize(serialized);
 
@@ -33,9 +31,8 @@ int main(int argc, char **argv) {
 	std::cout << "  Pointer Address: " << deserializedPtr << "\n";
 	std::cout << "  ID: " << deserializedPtr->id_ << "\n";
 	std::cout << "  Message: " << deserializedPtr->message_ << "\n";
-	std::cout << "  Value: " << deserializedPtr->value_ << "\n";
-	std::cout << std::endl;
-	
+	std::cout << "  Value: " << deserializedPtr->value_ << "\n\n";
+
 	if (originalPtr == deserializedPtr) {
 		std::cout << "Serialization successful!\n";
 	} else {
